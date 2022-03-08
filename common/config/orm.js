28de164317f884c939a4ad9dc85f8408ee690b67
Josse AS <gandalf.gris.89@gmail.com>
2022-03-08 14:54:46 -0500

    fix bug

--- common/config/orm.js
+++ common/config/orm.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const config_1 = require("@nestjs/config");
-const entitiesPath = process.env.ORM_ENTITIES ? `${process.env.ORM_ENTITIES}` : "dist/**/*.entity.js";
+const entitiesPath = process.env.ORM_ENTITIES ? `${process.env.ORM_ENTITIES}` : "**/*.entity.js";
 exports.default = (0, config_1.registerAs)('orm', () => ({
     type: process.env.ORM_CONNECTION,
     host: process.env.ORM_HOST,
